6364ba12a4d5d0f505d09ff4cbd91cf4b1fa826d
Imam Rafiif Arrazaan <58129889+rarrazaan@users.noreply.github.com>
2023-09-02 23:45:22 +0700

    Add files via upload

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://rafiif:password@localhost:5432/rafiif?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
